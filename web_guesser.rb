5651c27c9f87deb527fb524b607b2970ad380392
Daniel Glunz <dannyglunz@gmail.com>
2014-10-27 13:53:36 -0600

    I4: add color

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,19 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
+set :num, rand(100)
 
 get '/' do
   guess    = params['guess']
+  @color   = 'red'
   @message = check_guess(guess)
 
   erb :index
 end
 
 def check_guess(guess)
-  if guess.to_i == SECRET_NUMBER
+  if guess.to_i == settings.num
+    @color = 'green'
     "Correct!"
   else
-    guess.to_i > SECRET_NUMBER ? "Guess is too high." : "Guess is too low."
+    guess.to_i > settings.num ? "Guess is too high." : "Guess is too low."
   end
 end
